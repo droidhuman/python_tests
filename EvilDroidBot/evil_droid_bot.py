e758d00ab0a8d5015af8e64fcee4bf6c1528090f
droidhuman <iv@l1t.ru>
2021-06-08 15:02:40 +0300

    modified ../.gitignore & evil_droid_bot.py

--- EvilDroidBot/evil_droid_bot.py
+++ EvilDroidBot/evil_droid_bot.py
@@ -1,1 +1,22 @@
 # Evil Droid Bot
+
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+import settings
+
+
+def greet_user(update: Update, context: CallbackContext):
+	text = 'Вызван /start'
+	print(text)
+	update.message.reply_text(text)
+
+def main():
+	mybot = Updater(settings.APY_KEY)
+
+	dp = mybot.dispatcher
+	dp.add_handler(CommandHandler('start', greet_user))
+
+	mybot.start_polling()
+	mybot.idle()
+
+main()
